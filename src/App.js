e6747ba104becb40022be8e4a75b3fedc955ec43
revivesanjeev <revivesanjeev324@gmail.com>
2024-07-03 13:29:39 +0530

    stay logged in even after refreshing the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/profile" element={<UserProfile />} />
+          {!authCtx.isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
+          {authCtx.isLoggedIn && (
+            <Route path="/profile" element={<UserProfile />} />
+          )}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Layout>
     </Router>
